c8333ebab7cb7cb9bbb429cb969e3985da187094
Ryan Borowy <ryanborowy@Ryans-MacBook-Air.local>
2025-12-20 11:30:39 -0500

    add basic endpoints

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world")
+
+import express from "express";
+
+// Setup
+const app = express();
+const PORT = 3000;
+
+// Data
+let count=0;
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/increment", function(req, res) {
+    res.send(count.toString())
+    count++
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+})

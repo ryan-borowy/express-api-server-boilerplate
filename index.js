771f34b1b5394e1e90f93f0495d8f3ecd42e69a4
Ryan Borowy <ryanborowy@Ryans-MacBook-Air.local>
2026-01-03 15:57:20 -0500

    add get-name, set-name placeholder, html view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+import path from "path";
 
 // Setup
 const app = express();
@@ -7,15 +8,30 @@ const PORT = 3000;
 
 // Data
 let count=0;
+let serverName = "";
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send(`Hello world ${serverName}`);
 });
+
+app.get("/get-name", (req, res) => {
+    // Send an html form
+    res.sendFile(path.resolve("./views/index.html"))
+});
+
+app.get("/decrement", function() {
+
+})
 
 app.get("/increment", function(req, res) {
     res.send(count.toString())
     count++
+})
+
+app.post("/set-name", function(req, res) {
+    console.log("POST SET NAME was called")
+    console.log(req)
 })
 
 app.listen(PORT, () => {
